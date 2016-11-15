90a27aa825eee2d3a3de4d5eb2bb28840091af58
Devin Gaughan <tigrandrago@gmail.com>
2016-11-15 11:01:38 -0800

    need to change the sprite

--- batgame.js
+++ batgame.js
@@ -1,3 +1,5 @@
+
+//
 var Valuex = Number(document.getElementById("bat").getAttribute("x"));
 document.addEventListener("keydown", function(e) {
  if (e.keyCode == 37) {
@@ -9,3 +11,15 @@ document.addEventListener("keydown", function(e) {
   Valuex = Valuex+10
  }
  })
+
+ var Valuey = Number(document.getElementById("bat").getAttribute("y"));
+ document.addEventListener("keydown", function(e) {
+  if (e.keyCode == 38) {
+     document.getElementById("bat").setAttribute("y",Valuey-10)
+   Valuey = Valuey-10
+  }
+  else if (e.keyCode == 40){
+    document.getElementById("bat").setAttribute("y",Valuey+10)
+   Valuey = Valuey+10
+  }
+  })
