2acd5c83b22caf4ea1a8ada698a6a2da87d5382e
Devin Gaughan <tigrandrago@gmail.com>
2016-11-16 11:28:38 -0800

    working on food to collab with player

--- batgame.js
+++ batgame.js
@@ -1,5 +1,13 @@
+var home = document.getElementById('Grave')
+var batX
+var batY
+var itemX
+var itemY
+var width = 80
+var height = 80
 
-//
+
+//movemont left-right
 var Valuex = Number(document.getElementById("bat").getAttribute("x"));
 document.addEventListener("keydown", function(e) {
  if (e.keyCode == 37) {
@@ -11,7 +19,7 @@ document.addEventListener("keydown", function(e) {
   Valuex = Valuex+10
  }
  })
-
+//movement up-down
  var Valuey = Number(document.getElementById("bat").getAttribute("y"));
  document.addEventListener("keydown", function(e) {
   if (e.keyCode == 38) {
@@ -22,4 +30,21 @@ document.addEventListener("keydown", function(e) {
     document.getElementById("bat").setAttribute("y",Valuey+10)
    Valuey = Valuey+10
   }
+  })
+
+  //overlap
+  Grave.addEventListener(function (e) {
+    var bat = document.getElementById("bat")
+    var item = document.getElementById("item")
+
+       batX = Number(batX.getAttribute("x"))
+       batY = Number(batY.getAttribute("y"))
+
+    itemX = Number(itemX.getAttribute("x"))
+    itemY = Number(itemY.getAttribute("y"))
+
+    if(batX > itemX){
+      item.setAttribute("opacity", 0)
+    }
+
   })
