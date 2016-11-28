b1e43860cf116addf5724765d79ce89b4d6ec829
Devin Gaughan <tigrandrago@gmail.com>
2016-11-28 11:35:49 -0800

    overlap work!!

--- batgame.js
+++ batgame.js
@@ -5,12 +5,20 @@ document.addEventListener( "keydown", function (e) {
 //movemont left-right
 var itemX = Number(document.getElementById("item").getAttribute("x"));
 var itemY = Number(document.getElementById("item").getAttribute("y"));
-var width = 20;
-var height = 20;
+var width = 40;
+var height = 40;
 var Valuex = Number(document.getElementById("bat").getAttribute("x"));
 var Valuey = Number(document.getElementById("bat").getAttribute("y"));
 var bat = document.getElementById("bat");
 var item = document.getElementById("item");
+var soulsCollected = "";
+
+function randomNumberGenerator(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+var random = randomNumberGenerator(1, 300);
 
 //var batX = Number(document.getElementById("bat").getAttribute("x"));
 //var batY = Number(document.getElementById("bat").getAttribute("y"));
@@ -19,6 +27,7 @@ var item = document.getElementById("item");
 
 if (e.keyCode == 37) {
     document.getElementById("bat").setAttribute("x",Valuex-10)
+
 
  }
  else if (e.keyCode == 39){
@@ -38,9 +47,11 @@ if (e.keyCode == 37) {
 
  // one piece of code to check for overlap
 
-  if(Valuex > itemX && Valuex < itemX + width && Valuey > itemY && Valuey < itemY + height ){
-item.setAttribute("x", 100)
- var soulsCollected = soulsCollected += 1;
+  if(Valuex > itemX - width&& Valuex < itemX + width && Valuey > itemY -height&& Valuey - height< itemY + height ){
+item.setAttribute("x", random)
  console.log("work please!!!")
+  soulsCollected = soulsCollected += 1;
+  document.getElementById("text3").textContent = soulsCollected;
+
  }
  })
